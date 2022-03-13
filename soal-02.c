8b05e9472fa36c5251fce52fd958a002b9eb4689
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-13 20:09:54 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -20,18 +20,63 @@ typedef struct heater
 
 
 
+heater findState(struct heater heater1, float suhu)
+{
+    if (suhu <= heater1.setpoint - heater1.hysterisis/2.0)
+    {
+        heater1.state = 1;
+    }
+    else if (suhu >= heater1.setpoint + heater1.hysterisis/2.0)
+    {
+        heater1.state = 0;
+    }
+    else
+    {
+        heater1.state = heater1.state;
+    }
+    return heater1;
+}
+
 int main()
 {
-    //template input
+	heater heater1;
+    float temp;
+	float* suhu;
+	int length=0;
+    
 	printf("masukkan setpoint : ");
-	scanf("%f", &...);
+	scanf("%f", &heater1.setpoint);
 	printf("masukkan lebar hysterisis suhu : ");
-	scanf("%f", &...);
+	scanf("%f", &heater1.hysterisis);
 	printf("Masukkan suhu : ");
-	scanf("%f",&...);
+	scanf("%f",&temp);
+	while(temp != -1){
+		length += 1;
+		
+		if(length == 1){
+			suhu = (float*) malloc(sizeof(float));
+		}
+		else{
+			suhu = (float*) realloc(suhu, length * sizeof(float));
+		}
+		
+		suhu[length-1] = temp;
+		
+		printf("Masukkan suhu : ");
+		scanf("%f",&temp);
+	
+	}
 
-	//template output
-	printf("heater on!\n");
-	printf("heater off!\n");
+	for(int i=0;i<length;i++){
+		heater1 = findState(heater1, suhu[i]);
+		if (heater1.state == 1)
+		{
+			printf("heater on!\n");
+		}
+		else
+		{
+			printf("heater off!\n");
+		}
+	}
 
 }
